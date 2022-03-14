68d69613c1ac6b0d919f19d0a03fb35bb7d91f05
Isko21 <76448222+Isko21@users.noreply.github.com>
2022-03-14 16:50:37 +0100

    sub folders deleted

---
+++ build/geocoding/generated/source/buildConfig/release/com/baseflow/geocoding/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.baseflow.geocoding;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.baseflow.geocoding";
+  public static final String BUILD_TYPE = "release";
+}
