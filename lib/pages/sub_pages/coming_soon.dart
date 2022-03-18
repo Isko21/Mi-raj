ada399e7d1946660fa138acf26c956bfe45af3ac
Isko21 <76448222+Isko21@users.noreply.github.com>
2022-03-18 04:19:44 +0100

    surah container updated

--- lib/pages/sub_pages/coming_soon.dart
+++ lib/pages/sub_pages/coming_soon.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../../components/properties.dart';
 
@@ -13,38 +12,88 @@ class ComingSoon extends StatelessWidget {
     // ignore: unused_local_variable
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
-        backgroundColor: white,
         //appBar: AppBarCustom(change: () {}, title: title, page: Pages.settings),
-        body: SafeArea(
-          child: CustomScrollView(
-            slivers: [
-              SliverAppBar(
-                pinned: true,
-                snap: false,
-                floating: false,
-                iconTheme: IconThemeData(color: colorStr),
-                expandedHeight: height * 0.4,
-                flexibleSpace: FlexibleSpaceBar(
-                  title: Text(
-                    'Coming Soon',
-                    style: TextStyle(color: colorStr),
-                  ),
-                  background: Image.asset('assets/img/read_quran.jpg',
-                      fit: BoxFit.cover),
-                ),
+        body: Container(
+      color: color.withAlpha(50),
+      child: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            pinned: true,
+            snap: false,
+            floating: false,
+            iconTheme: IconThemeData(color: colorStr),
+            expandedHeight: height * 0.4,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(
+                'Coming Soon',
+                style: TextStyle(color: colorStr),
               ),
-              SliverList(
-                delegate: SliverChildBuilderDelegate((_, int index) {
-                  return ListTile(
-                    leading: Icon(FontAwesomeIcons.quran),
-                    title: Text(
-                      "Surah ${index + 1}",
-                    ),
-                  );
-                }, childCount: 20),
-              ),
-            ],
+              background:
+                  Image.asset('assets/img/read_quran.jpg', fit: BoxFit.cover),
+            ),
           ),
-        ));
+          SliverList(
+            delegate: SliverChildBuilderDelegate((_, int index) {
+              return Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+                child: Container(
+                    padding: EdgeInsets.only(right: 10),
+                    decoration: BoxDecoration(
+                        color: white,
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                    child: Row(children: [
+                      Container(
+                          margin: EdgeInsets.all(10),
+                          height: 70,
+                          width: 70,
+                          decoration: BoxDecoration(
+                              color: black,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(15)),
+                              image: DecorationImage(
+                                  opacity: 0.5,
+                                  fit: BoxFit.cover,
+                                  image: NetworkImage(
+                                      'https://wallpapercave.com/wp/WpDop9T.jpg'))),
+                          child: Center(
+                            child: Text(
+                              "Surah\n${index + 1}",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  fontSize: 17,
+                                  color: white,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          )),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Surah Al-Fatiha',
+                            style: TextStyle(
+                                fontSize: 19, fontWeight: FontWeight.bold),
+                          ),
+                          Text('The Opening', style: TextStyle(fontSize: 16))
+                        ],
+                      ),
+                      Expanded(
+                        child: Align(
+                          alignment: Alignment.centerRight,
+                          child: Text(
+                            'surotul al-fatiha',
+                            textDirection: TextDirection.rtl,
+                            textAlign: TextAlign.end,
+                          ),
+                        ),
+                      )
+                    ])),
+              );
+            }, childCount: 2),
+          ),
+        ],
+      ),
+    ));
   }
 }
