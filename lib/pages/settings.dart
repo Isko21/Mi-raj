f227e86ab1c7ccbf3f19978327c3ae9c5252ab58
Isko21 <76448222+Isko21@users.noreply.github.com>
2022-04-04 20:33:24 +0000

    Update settings.dart

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -3,10 +3,9 @@ import 'package:flutter/material.dart';
 class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Align(
-        child: Text('This is Settings page', style: TextStyle(fontSize: 30)),
-      ),
-    );
+    return Container(
+        child: Column(
+      children: [],
+    ));
   }
 }
