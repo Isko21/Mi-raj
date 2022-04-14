c5a8df1df3b212f7917e6db2f9143735de087775
Isko21 <76448222+Isko21@users.noreply.github.com>
2022-04-14 04:45:45 +0200

    Update pray_times.dart

--- lib/pages/pray_pages/pray_times.dart
+++ lib/pages/pray_pages/pray_times.dart
@@ -15,107 +15,103 @@ class PrayTimes extends StatefulWidget {
 class _PrayTimesState extends State<PrayTimes> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container(
-        color: color.withAlpha(50),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            SizedBox(
-              height: 10,
+    return Container(
+      color: color.withAlpha(50),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SizedBox(
+            height: 10,
+          ),
+          Image.asset(
+            'assets/img/pray_day.png',
+            height: MediaQuery.of(context).size.height * 0.25,
+          ),
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: Container(
+              decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 2),
+                    )
+                  ],
+                  color: white,
+                  borderRadius: BorderRadius.all(Radius.circular(20))),
+              child: Column(children: [
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: WidgetAnimator(
+                    child: Text(
+                      "Today's prayer times",
+                      textAlign: TextAlign.start,
+                      style:
+                          TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                WidgetAnimator(
+                    child: LinePrayer(
+                        pray: getTime(prayerTimes.fajr), title: 'Fajr')),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  child: Divider(
+                    color: black,
+                  ),
+                ),
+                WidgetAnimator(
+                    child: LinePrayer(
+                        pray: getTime(prayerTimes.sunrise), title: 'Sunrise')),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  child: Divider(
+                    color: black,
+                  ),
+                ),
+                WidgetAnimator(
+                    child: LinePrayer(
+                        pray: getTime(prayerTimes.dhuhr), title: 'Dhuhr')),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  child: Divider(
+                    color: black,
+                  ),
+                ),
+                WidgetAnimator(
+                    child: LinePrayer(
+                        pray: getTime(prayerTimes.asr), title: 'Asr')),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  child: Divider(
+                    color: black,
+                  ),
+                ),
+                WidgetAnimator(
+                    child: LinePrayer(
+                        pray: getTime(prayerTimes.maghrib), title: 'Maghrib')),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  child: Divider(
+                    color: black,
+                  ),
+                ),
+                WidgetAnimator(
+                    child: LinePrayer(
+                        pray: getTime(prayerTimes.isha), title: 'Isha')),
+              ]),
             ),
-            Image.asset(
-              'assets/img/pray_day.png',
-              height: MediaQuery.of(context).size.height * 0.25,
-            ),
-            Padding(
-              padding: EdgeInsets.all(20),
-              child: Container(
-                decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 5,
-                        blurRadius: 7,
-                        offset: Offset(0, 2),
-                      )
-                    ],
-                    color: white,
-                    borderRadius: BorderRadius.all(Radius.circular(20))),
-                child: Column(children: [
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: WidgetAnimator(
-                      child: Text(
-                        "Today's prayer times",
-                        textAlign: TextAlign.start,
-                        style: TextStyle(
-                            fontSize: 17, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ),
-                  WidgetAnimator(
-                      child: LinePrayer(
-                          pray: getTime(prayerTimes.fajr), title: 'Fajr')),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                    child: Divider(
-                      color: black,
-                    ),
-                  ),
-                  WidgetAnimator(
-                      child: LinePrayer(
-                          pray: getTime(prayerTimes.sunrise),
-                          title: 'Sunrise')),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                    child: Divider(
-                      color: black,
-                    ),
-                  ),
-                  WidgetAnimator(
-                      child: LinePrayer(
-                          pray: getTime(prayerTimes.dhuhr), title: 'Dhuhr')),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                    child: Divider(
-                      color: black,
-                    ),
-                  ),
-                  WidgetAnimator(
-                      child: LinePrayer(
-                          pray: getTime(prayerTimes.asr), title: 'Asr')),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                    child: Divider(
-                      color: black,
-                    ),
-                  ),
-                  WidgetAnimator(
-                      child: LinePrayer(
-                          pray: getTime(prayerTimes.maghrib),
-                          title: 'Maghrib')),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                    child: Divider(
-                      color: black,
-                    ),
-                  ),
-                  WidgetAnimator(
-                      child: LinePrayer(
-                          pray: getTime(prayerTimes.isha), title: 'Isha')),
-                ]),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
-              child: AutoSizeText(
-                  'The best act of man is namaz.\nThe best namaz is the timely namaz.',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: colorStr)),
-            )
-          ],
-        ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+            child: AutoSizeText(
+                'The best act of man is namaz.\nThe best namaz is the timely namaz.',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: colorStr)),
+          )
+        ],
       ),
     );
   }
